88509be28e5f0c26316039f982a528b2062ff6c8
Mateus Daniel <mano.mateus2012@gmail.com>
2017-05-18 19:56:45 -0300

    Inicio projeto

---
+++ TRABALHO_POO2/src/main/java/com/poo2/TrabalhoPoo2Application.java
@@ -0,0 +1,12 @@
+package com.poo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPoo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPoo2Application.class, args);
+	}
+}
